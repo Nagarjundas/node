9504bf35989de892597f7f86efcbee53e8b7fb5e
NAGARJUN <dasnagarjun1994@gmail.com>
2021-04-07 20:26:33 +0530

    c3

--- demo.js
+++ demo.js
@@ -55,3 +55,69 @@
 //     res.write(uc.upperCase("this is a example of upper case pacakge install"));
 //     res.end();
 // }).listen(4000)
+
+// sending mail to a account 
+
+// var nodemailer = require('nodemailer');
+// var transport = nodemailer.createTransport({
+
+//     host: 'smtp.gmail.com',
+//     port: 587,
+//     secure: false,
+//     require: true,
+//     auth: { user: 'nagarjun.das59@gmail.com', pass: 'Aaaaaaaaaa10' }
+
+
+// });
+// var mailOption = {
+
+//     from: 'nagarjun.das59@gmail.com',
+//     to: 'dasnagarjun007@gmail.com',
+//     subject: 'test node',
+//     text: 'Hello yes it is working '
+
+// }
+// transport.sendMail(mailOption, function (err, info) {
+//     if (error) {
+//         console.log(error);
+//     }
+//     else {
+//         console.warn('email sent', info.response);
+//     }
+// }
+
+
+// )
+
+
+// var http = require('http');
+// http.createServer(function (req, res) {
+
+//     res.write("hello this is a example of testing in youtube and nodemon tutorial");
+//     res.end();
+// }).listen(4500)
+
+// READING FILE SYSTEM
+
+// var http = require('http');
+// var fs = require('fs');
+
+// http.createServer(function(req, res) {
+
+//     fs.readFile('demo.html',function(err,data){
+
+//         res.writeHead(200,{'Content-Type': 'text/html'})
+//         res.write(data);
+//         return res.end();
+//     })
+// }).listen(4500);
+
+
+// EVENTS in nodejs.
+
+// var fs = require('fs');
+// var rs = fs.createReadStream('./demo.js');
+// rs.on('open',function(){
+
+//     console.log("demo file is open");
+// });
